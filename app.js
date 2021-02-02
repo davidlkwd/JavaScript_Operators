8c606eba5af19e81273af263fd835ab74f4b597c
David Herrbach <dave@ihmrinc.com>
2021-02-01 21:36:45 -0800

    Initial Commit

--- app.js
+++ app.js
@@ -8,12 +8,74 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+let a1 = 11;
+let str = "11";
+let str2= "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+let a1PlusStr = a1 + str;
+let a1PlusStr2 = a1 + str2;
+let a1PlusIsPresent = a1 + isPresent;
+let a1PlusFirstName = a1 + firstName;
+let a1PlusLastName = a1 + lastName;
+
+
+
+
+
+
+console.log(a1PlusStr);
+console.log(a1PlusStr2);
+console.log(a1PlusIsPresent);
+console.log(a1PlusFirstName);
+console.log(a1PlusLastName);
+
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let a2 = 5;
+let str1A = "5";
+let str2A = "five";
+let isPresent1 = false;
+let firstName1 = "Robin";
+let lastName1 = "Williams";
+
+let a2EqualsStr1A = a2 == str1A;
+let a2StrictlyEqualsStr1A = a2 === str1A;
+let notEqualIsPresent1 = !isPresent1;
+let and = "eleven" == str2A && a2 >= str1A;
+let or = !isPresent1 || isPresent1;
+let value1 = 0 == false;
+let value2 = 0 === false;
+let value3 = 0 != false;
+let value4 = 0 !== false;
+
+console.log(a2EqualsStr1A);
+console.log(a2StrictlyEqualsStr1A);
+console.log(notEqualIsPresent1);
+console.log(and);
+console.log(or);
+console.log(value1)
+console.log(value2);
+console.log(value3);
+console.log(value4);
